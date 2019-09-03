001ddaaf4c992e21f20827bec9bdab6b14d1c7b9
Elham Aryanpur <elhamaryanpur5@gmail.com>
2019-09-03 17:40:49 +0430

    Core Of Engine is finished

--- Server.py
+++ Server.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, render_template, redirect
 from json import loads, dumps
+from static.lib.compiler import Compile
 import yaml
 import os
 
 app = Flask(__name__)
+cur_dir = os.getcwd()
+project_dir = os.path.join(cur_dir, "static")
+project_dir = os.path.join(project_dir, "projects")
+names_data_path = os.path.join(project_dir, "name.data")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -11,16 +16,64 @@ def index():
 
 @app.route("/save", methods=["POST"])
 def save():
+    data = request.form["config"]
+    config = yaml.load(data)
+
+    with open(names_data_path, "r") as f:
+        names_data = f.read()
+        names_data = loads(names_data)
     
-    data = request.form["data"]
-    data = yaml.load(data)
+    final = {
+        "config": request.form["config"],
+        "preload": request.form["preload"],
+        "create": request.form["create"],
+        "update": request.form["update"]
+    }
+
+    names_data[config["name"]] = final
+    
+    with open(names_data_path, "w") as f:
+        f.write(dumps(names_data))
 
     return redirect("/")
 
-@app.route("/load/<name>", methods=["GET"])
-def load(name):
-    d = dumps({"lala": "koko"})
-    return d
+@app.route("/load", methods=["POST"])
+def load():
+
+    name = request.form["name"]
+
+    with open(names_data_path, "r") as f:
+        names_data = f.read()
+        names_data = loads(names_data)
+    
+    try:
+        data = names_data[name]
+        return dumps(data)
+    except Exception as e:
+        print(e)
+        return "<script>alert('{} DOES NOT EXIST YET!')</script>".format(name)
+
+@app.route("/run", methods=["POST"])
+def run():
+
+    config = request.form["config"]
+    config = yaml.load(config)
+
+    with open(names_data_path, "r") as f:
+        names_data = f.read()
+        names_data = loads(names_data)
+    
+    data = names_data[config["name"]]
+    
+    c = Compile(config, data['preload'], data['create'], data['update'])
+    c = c.compile()
+
+    name = os.path.join(project_dir, str(config["name"] + ".html"))
+
+    with open(name, "w") as f:
+        f.write(c)
+
+    return "SUCCESS"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/lib/compiler.py
+++ static/lib/compiler.py
@@ -1,3 +1,5 @@
+import os
+
 class Compile():
     def __init__(self, config, preload, create, update):
         self.config = config
@@ -13,25 +15,51 @@ class Compile():
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="engine.js"></script>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+            background: #121920;
+        }
+    </style>
     <title>""" + self.name + """</title>
 </head>
 <body>
 <script>\n\n"""
+
+        path = os.path.join(os.getcwd(), "static")
+        path = os.path.join(path, "js")
+        path = os.path.join(path, "engine.js")
+        with open(path, "r") as f:
+            engine = f.read()
+
+        first_parts += str(engine)
+        first_parts += "\n\n\n"
+
         last_parts = """\n</script>
 </body>
 </html>"""
 
-        config = "var game = new Phaser.Game({\ntype: Phaser.AUTO, width: " + str(self.config["width"]) + \
+        config = "var config = {\ntype: Phaser.AUTO, width: " + str(self.config["width"]) + \
             ", height: " + str(self.config["height"]) + ", "
         
-        for i in self.config["else"].keys():
-            config += str(i) + ": " + str(self.config["else"][i]) + ", "
+        if "else" in self.config:
+            for i in self.config["else"].keys():
+                config += str(i) + ": " + str(self.config["else"][i]) + ", "
+        else:
+            pass
 
-        config_last = "scene: { preload: preload, create: create, update: update });"
+        config_last = "scene: { preload: preload, create: create, update: update }\n}\n"
         config += config_last
 
         first_parts += config
 
-c = Compile({"name": "MyAwesomeGame", "width": 800, "height": 600, "else": {}}, [], [], [])
-c.compile()
+        first_parts += "\nvar game = new Phaser.Game(config);"
+        
+        first_parts += "\nfunction preload(){\n" + self.preload + "\n}"
+        first_parts += "\nfunction create(){\n" + self.create + "\n}"
+        first_parts += "\nfunction update(){\n" + self.update + "\n}"
+        
+        first_parts += last_parts
+
+        return first_parts
