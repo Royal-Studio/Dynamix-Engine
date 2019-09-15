6bd7b46db5374d3825bbd355511ab4ff7c9b9d16
Elham Aryanpur <elhamaryanpur5@gmail.com>
2019-09-15 23:41:24 +0430

    some stuff changed and some added

---
+++ 3D/engine.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace std;
+using namespace glm;
+
+int main(){
+
+    // Initialise GLFW
+    glewExperimental = true; // Needed for core profile
+    if( !glfwInit() ){
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing! This was easier than I thought!
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+    window = glfwCreateWindow( 1000, 800, "Hello World", NULL, NULL);
+    if( window == NULL ){
+        fprintf( stderr, "FAILED to open GLFW window. If you have an Intel GPU, \
+         they are not 3.3 compatible.\n" );
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window); // Init GLEW
+    glewExperimental = true; // Needed in core profile
+
+    if ( glewInit() != GLEW_OK ){
+        fprintf( stderr, "Failed To Initialize GLEW\n" );
+        return -1;
+    }
+
+    // Ensure we can capture escape key being pressed below
+    glfwSetInputMode( window, GLFW_STICKY_KEYS, GL_TRUE );
+
+    do {
+        // Clear the screen. It can cause flickering, so it's there nonetheless.
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        // Draw nothing...
+
+        // Swap Buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && 
+    glfwWindowShouldClose(window) == 0 );
+
+    return 0;
+}
