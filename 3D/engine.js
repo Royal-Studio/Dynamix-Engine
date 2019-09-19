f840f962fed50f03854f9456f5743ba745cd6a9d
Elham Aryanpur <elhamaryanpur5@gmail.com>
2019-09-19 17:05:27 +0430

    some 3D tests done on web and native and logos added

--- 3D/engine.js
+++ 3D/engine.js
@@ -11,29 +11,27 @@ var engine = new EGB.Engine(canvas, true);
 function createScene(){
 
     var scene = new EGB.Scene(engine);
+    scene.gravity = new EGB.Vector3(0, -0.2, 0);
+    scene.collisionEnabled = true;
 
     var camera = new EGB.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 0, 0), scene);
     camera.attachControl(canvas, true);
     camera.speed = 0.2;
     camera.angularSensibility = 5000;
+    camera.applyGravity = true;
+    // Set the ellipsoid around the camera
+    camera.ellipsoid = new EGB.Vector3(0.5, 0.5, 0.5);
+    //camera.ellipsoidOffset = 0.5;
+    camera.checkCollisions = true;
+    camera._needMoveForGravity = true;
 
     var light1 = new EGB.HemisphericLight('light1', new EGB.Vector3(1,1,0), scene);
     var light2 = new EGB.PointLight('light2', new EGB.Vector3(0,1,-1), scene);
 
     var plane = EGB.MeshBuilder.CreateBox("myBox", {width: 15, height: 0.1, depth: 15}, scene);
-    plane.translate(EGB.Axis.Y, -2, EGB.Space.WORLD);
-
-    //EGB.SceneLoader.Append('./', 'test.obj', scene, function (scene) {});
-
-    scene.gravity = new EGB.Vector3(0, -0.2, 0);
-    camera.applyGravity = true;
-    // Set the ellipsoid around the camera
-    camera.ellipsoid = new EGB.Vector3(1, 1, 1);
-
-    scene.collisionEnabled = true;
-    camera.checkCollisions = true;
-
+    plane.position.y = -1;
     plane.checkCollisions = true;
+    plane.freezeWorldMatrix();
 
     return scene;
 
